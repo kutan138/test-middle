5315b4774ad6f6fd474d9522bba524ae32bb214f
Tan <letutan500@gmail.com>
2024-10-12 22:17:21 +0700

    Remove build

--- next.config.cjs
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const nextBuildId = require("next-build-id");
-
-const nextConfig = {
-  generateBuildId: () => nextBuildId({ dir: __dirname, describe: true }),
-};
-
-export default nextConfig;
---
+++ next.config.js
@@ -0,0 +1,6 @@
+const nextConfig = {
+  trailingSlash: true,
+  generateBuildId: () => "release/v1",
+};
+
+export default nextConfig;
