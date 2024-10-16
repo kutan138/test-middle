cce3a3ec352be4021e085d1dff58336af4313114
Tan <letutan500@gmail.com>
2024-10-16 10:25:08 +0700

    AAA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const nextBuildId = require("next-build-id");
 module.exports = {
   trailingSlash: true,
   generateBuildId() {
     // NOTE: describe=true の場合、タグが付与されているとタグ、そうでなければ commit hash
-    return nextBuildId({ dir: __dirname, describe: true });
+    return "release/test1";
   },
 };
